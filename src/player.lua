c9922d3adc4bc03fb6a91eb17f5165bfe966dfe4
James (Laptop) <james.mckibbin@mymail.champlain.edu>
2025-01-23 21:42:48 -0500

    no jitter

--- src/player.lua
+++ src/player.lua
@@ -5,7 +5,7 @@ local function player_update(player, dt)
         player.target.x = love.mouse.getX() - (player.size.x / 2)
         player.target.y = love.mouse.getY() - (player.size.y / 2)
     end
-    if player.pos.x ~= player.target.x and player.pos.y ~= player.target.y then
+    if math.abs(player.target.x - player.pos.x) > 0.1 and math.abs(player.target.y - player.pos.y) > 0.1 then
         local dir = vec2.new(player.target.x - player.pos.x, player.target.y - player.pos.y)
         player.pos = player.pos + (vec2.normalize(dir) * player.speed * dt)
     end
@@ -19,7 +19,7 @@ local function player_new()
     return {
         pos = vec2.new(50, 50),
         size = vec2.new(50, 50),
-        target = vec2.new(0, 0),
+        target = vec2.new(50, 50),
         speed = 100,
         update = player_update,
         draw = player_draw,
