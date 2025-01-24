b06224993b9b144a855616763c1d9bacf5cee3c0
James (Laptop) <james.mckibbin@mymail.champlain.edu>
2025-01-23 22:13:09 -0500

    centered room art

--- src/main.lua
+++ src/main.lua
@@ -3,6 +3,9 @@ local player
 
 function love.load()
     player = player_new()
+    love.graphics.setBackgroundColor(0.2, 0.2, 0.2)
+    bg = love.graphics.newImage("assets/bgs/test_room.png")
+    love.graphics.scale(2, 2)
 end
 
 function love.update(dt)
@@ -10,5 +13,7 @@ function love.update(dt)
 end
 
 function love.draw()
+    love.graphics.draw(bg, (love.graphics.getWidth()/2)-(bg:getWidth()/2),
+                           (love.graphics.getHeight()/2)-(bg:getHeight()/2))
     player:draw()
 end
